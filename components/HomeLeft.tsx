53c39ea98ee7cf054b467d35989491005ef55bba
chandra <101719497+chandra1899@users.noreply.github.com>
2023-09-15 20:24:28 +0530

    front left and right completed

--- components/HomeLeft.tsx
+++ components/HomeLeft.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
+import Image from 'next/image'
+
+const MatchComponent=()=>{
+  return (
+    <div className='w-[100%] h-[60px] rounded-md flex flex-row justify-around items-center cursor-pointer hover:bg-[#3b3b3be6] bg-[#302c2ce6] transition-all ease-in-out delay-[20ms] my-3'>
+      <div className='flex flex-row max-w-[50%] justify-center items-center'>
+      <Image
+      height={40}
+      width={40}
+      src={'/profile.png'}
+      alt='profle'
+      className='rounded-full mx-2'
+      />
+     <p className=' break-all text-green-600'>c4746665@gmail.com</p>
+      </div>
+
+      <div>
+      <Image
+      height={40}
+      width={40}
+      src={'/battle.png'}
+      alt='profle'
+      className='rounded-full'
+      />
+      </div>
+
+      <div className='flex flex-row max-w-[50%] justify-center items-center'>
+      <Image
+      height={40}
+      width={40}
+      src={'/profile.png'}
+      alt='profle'
+      className='rounded-full mx-2'
+      />
+     <p className=' break-all text-red-600'>c4746665@gmail.com</p>
+      </div>
+    </div>
+  )
+}
 
 const HomeLeft = () => {
   return (
-    <div className='h-[100vh] w-[45%] bg-[#222222e6] text-white'>
-      HomeLeft
+    <div className='h-[100vh] w-[45%] bg-[#222222e6] text-white p-4 overflow-y-scroll'>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
+      <MatchComponent/>
     </div>
   )
 }
--- components/HomeRight.tsx
+++ components/HomeRight.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HomeRight = () => {
   return (
     <div className='h-[100vh] w-[27%] bg-[#222222e6] text-white'>
-     <div className='w-[100%] h-[40%] bg-red-400'></div>
+     <div className='w-[100%] h-[40%] flex flex-row justify-center items-center'>
+      <div className='w-[30%] mx-3'>
+      <Image
+      height={100}
+      width={100}
+      src={'/profile.png'}
+      alt='profle'
+      className='rounded-full'
+      />
+      </div>
+      <div className='flex flex-col w-[60%]'>
+     <p>Name:Chandra Kumar</p>
+     <p>Email: c4746665@gmail.com</p>
+      </div>
+     </div>
+
      <div className='flex flex-col justify-center items-center p-5'>
         <p className='h-[45px] w-[80%] flex justify-center items-center rounded-md cursor-pointer hover:bg-[#736767e6] bg-[#4b4545e6] m-2 transition-all ease-in-out delay-[20ms]'>Create Game</p>
         <p className='h-[45px] w-[80%] flex justify-center items-center rounded-md cursor-pointer hover:bg-[#736767e6] bg-[#4b4545e6] m-2 transition-all ease-in-out delay-[20ms]'>Play with Friend</p>
